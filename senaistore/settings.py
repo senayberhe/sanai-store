b3f8342dfb7278f56db01d087c36d6b470c98cb8
Senay Berhe <tsionberhe@gmail.com>
2019-12-20 23:30:16 -0800

    added files

--- senaistore/settings.py
+++ senaistore/settings.py
@@ -14,6 +14,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core'
 ]
 
@@ -63,6 +67,15 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+
+SITE_ID = 1
 
 if ENVIRONMENT == 'production':
     DEBUG = False
